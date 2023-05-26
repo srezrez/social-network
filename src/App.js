a2b92d089b01d02f329cdf012c0dd0cf440c3543
Svetlana Reznichenko <srezrez@mail.com>
2023-05-26 15:23:40 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      {/* <Profile /> */}
       <div className='app-wrapper-content'>
-        <Dialogs />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
