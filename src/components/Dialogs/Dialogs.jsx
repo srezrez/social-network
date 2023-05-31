3be54153499dda37e992000abbb30db6e1f041b4
Svetlana Reznichenko <srezrez@mail.com>
2023-05-31 16:31:49 +0300

    edit dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,29 @@
-import styles from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/1'> Egor </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'> Sveta </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'> Diana </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'> Nastya </NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}> Hi </div>
+                <div className={s.message}> How are you? </div>
+                <div className={s.message}> Hello </div>
+            </div>
+        </div>
     );
 }
 
