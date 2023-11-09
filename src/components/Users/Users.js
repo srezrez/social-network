ae0711e33b9c5b98f824ba2d63ea3f1c0da97da7
Svetlana Reznichenko <srezrez@mail.com>
2023-11-09 16:37:22 +0300

    added follow-unfollow

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -29,8 +29,36 @@ let Users = (props) => {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                                ? <button onClick={() => {
+
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY" : "0d08b5d7-b900-4b65-b19c-5dcc568febc1"
+                                        }
+                                    })
+                                    .then(response => {
+                                    if (response.data.resultCode === 0) {
+                                        props.unfollow(u.id)
+                                    }
+                                });
+
+                                }}>Unfollow</button>
+                                : <button onClick={() => { 
+
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY" : "0d08b5d7-b900-4b65-b19c-5dcc568febc1"
+                                        }
+                                    })
+                                    .then(response => {
+                                    if (response.data.resultCode === 0) {
+                                        props.follow(u.id)
+                                    }
+            });
+
+                                    }}>Follow</button>}
                         </div>
                     </span>
                     <span>
